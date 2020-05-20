2cab7287fb9ba46b46913b6b17566c9f351d0428
Renzoo <lbafable@up.edu.ph>
2020-05-21 02:46:54 +0800

    Updates

---
+++ precache-manifest.51fe0e225d9386ed78eab78204307950.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c40c2bdd324bc467e98c4c756be96b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d4ed160f166d05ef106",
+    "url": "/static/css/main.dd9770a6.chunk.css"
+  },
+  {
+    "revision": "3b68345a3ecdb50ff940",
+    "url": "/static/js/2.45f5a806.chunk.js"
+  },
+  {
+    "revision": "0ccf9295f6767f64d9073d581a6bf4d2",
+    "url": "/static/js/2.45f5a806.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d4ed160f166d05ef106",
+    "url": "/static/js/main.7abdd21a.chunk.js"
+  },
+  {
+    "revision": "ed77dc18feb333ecf6c6",
+    "url": "/static/js/runtime-main.1d6181fc.js"
+  },
+  {
+    "revision": "6bb7f0a6e6e2cf14e5f7840c183af491",
+    "url": "/static/media/author-image.6bb7f0a6.jpg"
+  },
+  {
+    "revision": "65a4714643de7903670c1edd4f6b92de",
+    "url": "/static/media/header-bg.65a47146.jpg"
+  }
+]);
